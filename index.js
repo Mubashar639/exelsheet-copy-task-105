8d82ac5f64184aac77b00c17c75fcab8393be466
Mubashar639 <hmubashar639@gmail.com>
2021-08-22 21:32:00 +0500

    final understanding requirments

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const Excel = require("exceljs");
 
-async function copyExcel() {
+const filePath = `${__dirname}/My_Data/My_TEST_Copy.xlsx`;
+const toBeCopy = "About_Me_Dtls1";
+
+const copiedVersion = "Copied_Version";
+
+/**
+ *return void 
+ just console the the notification message file has been changed 
+ */
+
+(async () => {
   /**
    * create the work book instance for target
    */
@@ -9,12 +19,12 @@ async function copyExcel() {
   /**
    * provide the target file with hardcoded name
    */
-  targetWorkbook = await targetWorkbook.xlsx.readFile("my_data/target.xlsx");
+  targetWorkbook = await targetWorkbook.xlsx.readFile(filePath);
 
   /**
    * provide the worksheet on which new data copy
    */
-  const targetWorksheet = targetWorkbook.getWorksheet(`Sheet1`);
+  const targetWorksheet = targetWorkbook.getWorksheet(copiedVersion);
 
   // you can add new sheet as well.
   //   here Sheet1 is hardcoded as you allow but but we attact from like that
@@ -28,13 +38,12 @@ async function copyExcel() {
   /**
    * give the name to file which you want read
    */
-  sourceWorkbook = await sourceWorkbook.xlsx.readFile("my_data/source.xlsx");
+  sourceWorkbook = await sourceWorkbook.xlsx.readFile(filePath);
 
   /**
    * gave the sheet name
    */
-  const sourceWorksheet = sourceWorkbook.getWorksheet("Sheet1");
-
+  const sourceWorksheet = sourceWorkbook.getWorksheet(toBeCopy);
   /**
    * load into memory and create file pointer
    */
@@ -43,17 +52,19 @@ async function copyExcel() {
      * add row condition in description you wanna 500
      */
     if (rowNumber > 500) return;
-    var targetRow = targetWorksheet.getRow(rowNumber);
+    const targetRow = targetWorksheet.getRow(rowNumber);
     row.eachCell({ includeEmpty: false }, (cell, cellNumber) => {
       targetRow.getCell(cellNumber).value = cell.value;
+      targetRow.getCell(cellNumber).style = cell.style;
     });
     row.commit();
   });
   /**
    * write into file
    */
-  await targetWorkbook.xlsx.writeFile("my_data/target.xlsx");
-  console.log("file is copied in target.xlsx");
-}
+  await targetWorkbook.xlsx.writeFile(filePath);
 
-copyExcel();
+  console.log(
+    `file sheet has been copied from ${toBeCopy} sheet to ${copiedVersion} on path ${filePath}`
+  );
+})();
